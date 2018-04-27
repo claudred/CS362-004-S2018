025114cb7a92b890bc00ea150745958b2811f032
Claudia RodriguezSchroeder <rodricla@flip1.engr.oregonstate.edu>
2018-04-26 18:04:00 -0700

    added a small test

---
+++ projects/rodricla/dominion/small_test.c
@@ -0,0 +1,22 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+#include <stdlib.h>
+#include <stdio.h>
+//function that tests the rv=return value of a shuffle deck and compares it to the deckCount of a specific player
+void rv_shuffle_deckCount(int rv, int rp);
+int main()
+{
+	
+	return 0;
+}
+void rv_shuffle_deckCount(int rv, int rp)
+{
+	
+	if (rv==-1)
+	{
+		printf("SUCCESS: function noted that player %i deck was empty, and returned %i\n", rp, rv);
+	}
+	
+}
+
