197300ad7e4a4edd339152d98e70aa000052cdf5
Claudia RodriguezSchroeder <rodricla@flip3.engr.oregonstate.edu>
2018-04-27 16:15:03 -0700

    cleaned up

--- projects/rodricla/dominion/unittest1.c
+++ projects/rodricla/dominion/unittest1.c
@@ -85,35 +85,3 @@ int check_shuffle(int player, struct gameState *one, struct gameState *two)
 	}
 	return successCount;
 }
-/*
-int shuffle(int player, struct gameState *state) {
- 
-
-	int newDeck[MAX_DECK];
-	int newDeckPos = 0;
-	int card;
-	int i;
-
-	if (state->deckCount[player] < 1)
-		return -1;
-	qsort ((void*)(state->deck[player]), state->deckCount[player], sizeof(int), compare); 
-	//SORT CARDS IN DECK TO ENSURE DETERMINISM!
-
-	while (state->deckCount[player] > 0) {
-		card = floor(Random() * state->deckCount[player]);
-		newDeck[newDeckPos] = state->deck[player][card];
-		newDeckPos++;
-		for (i = card; i < state->deckCount[player]-1; i++) {
-			state->deck[player][i] = state->deck[player][i+1];
-		}
-		state->deckCount[player]--;
-	}
-	for (i = 0; i < newDeckPos; i++) {
-		state->deck[player][i] = newDeck[i];
-		state->deckCount[player]++;
-	}
-
-	return 0;
-}
-
-*/
